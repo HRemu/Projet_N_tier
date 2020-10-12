51e1a9426588bb257c3f85a361c12b63f8e3477c
Arnaud <56339879+ArnaudGuibert@users.noreply.github.com>
2020-10-12 09:11:48 +0200

    nouveau script base SQL

--- database/script_base.sql
+++ database/script_base.sql
@@ -1,33 +1,50 @@
 CREATE DATABASE stageetudiant;
 
+USE stageetudiant;
+
+
+CREATE TABLE GRADYEAR(
+   gradyear_id INT AUTO_INCREMENT NOT NULL,
+   label VARCHAR(20) NOT NULL,
+   PRIMARY KEY ( gradyear_id )
+);
+
+
 CREATE TABLE STUDENT(
-   id INT AUTO_INCREMENT NOT NULL,
+   student_id INT AUTO_INCREMENT NOT NULL,
    fname VARCHAR(50) NOT NULL,
    lname VARCHAR(50) NOT NULL,
-   PRIMARY KEY( id )
+   gradyear_id INT NOT NULL,
+   PRIMARY KEY( student_id ),
+   FOREIGN KEY ( gradyear_id ) REFERENCES GRADYEAR( gradyear_id )
 );
+
 
 CREATE TABLE COUNTRY(
-   id INT AUTO_INCREMENT NOT NULL,
-   name VARCHAR(50) NOT NULL,
-   PRIMARY KEY( id )
+   country_id INT AUTO_INCREMENT NOT NULL,
+   country_name VARCHAR(50) NOT NULL,
+   PRIMARY KEY( country_id )
 );
+
 
 CREATE TABLE CITY(
-   id INT AUTO_INCREMENT NOT NULL,
-   name VARCHAR(50) NOT NULL,
+   city_id INT AUTO_INCREMENT NOT NULL,
+   city_name VARCHAR(50) NOT NULL,
    country_id INT NOT NULL,
-   PRIMARY KEY ( id ),
-   FOREIGN KEY ( country_id ) REFERENCES COUNTRY( id )
+   PRIMARY KEY ( city_id ),
+   FOREIGN KEY ( country_id ) REFERENCES COUNTRY( country_id )
 );
 
+
 CREATE TABLE INTERNSHIP(
-   id INT AUTO_INCREMENT NOT NULL,
+   internship_id INT AUTO_INCREMENT NOT NULL,
    student_id INT NOT NULL,
    city_id INT NOT NULL,
    start_date DATE NOT NULL,
    end_date DATE,
-   PRIMARY KEY ( id ),
-   FOREIGN KEY ( student_id ) REFERENCES STUDENT( id ),
-   FOREIGN KEY ( city_id ) REFERENCES CITY( id )
+   PRIMARY KEY ( internship_id ),
+   FOREIGN KEY ( student_id ) REFERENCES STUDENT( student_id ),
+   FOREIGN KEY ( city_id ) REFERENCES CITY( city_id )
 );
+
+
