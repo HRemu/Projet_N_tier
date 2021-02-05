41c07f12b3e423d35db8a883f638f99b9fd2744c
Arnaud <56339879+ArnaudGuibert@users.noreply.github.com>
2021-02-05 17:16:07 +0100

    modifs modèles / formulaires / autres

--- DjangoTest/mysite/mysite/models.py
+++ DjangoTest/mysite/mysite/models.py
@@ -3,32 +3,35 @@ from django.db import models
 # Create your models here.
 
 class Gradyear(models.Model):
-   gradyear_id  = models.AutoField(primary_key=True) 
-   label =models.CharField(max_length=20, null = False)
+    gradyear_id  = models.AutoField(primary_key = True) 
+    label = models.CharField(max_length = 20, null = False)
+
 
 class Student(models.Model):
-    student_id = models.AutoField(primary_key=True) 
-    fname = models.CharField(max_length=50, null = False)
-    lname =models.CharField(max_length=50, null = False) 
-    gradyear_id =models.ForeignKey(Gradyear, on_delete=models.CASCADE)
+    student_id = models.AutoField(primary_key = True) 
+    fname = models.CharField(max_length = 50, null = False)
+    lname = models.CharField(max_length = 50, null = False) 
+    email = models.EmailField(max_length = 100, unique = True, null = False)
+    gradyear_id = models.ForeignKey(Gradyear, on_delete = models.CASCADE)
+
 
 class Country(models.Model):
-    country_id = models.AutoField(primary_key= True)
-    country_name =models.CharField(max_length=50, null = False) 
+    country_id = models.AutoField(primary_key = True)
+    country_name = models.CharField(max_length = 50, null = False) 
 
 
 class City(models.Model):
-    city_id =models.AutoField(primary_key= True)
-    city_name = models.CharField(max_length=50, null = False)
-    longitude = models.CharField(max_length=10, null = False)
-    latitude =  models.CharField(max_length=10, null = False)
-    country_id = models.ForeignKey(Country, on_delete=models.CASCADE)
+    city_id = models.AutoField(primary_key = True)
+    city_name = models.CharField(max_length = 50, null = False)
+    longitude = models.CharField(max_length = 10, null = False)
+    latitude =  models.CharField(max_length = 10, null = False)
+    country_id = models.ForeignKey(Country, on_delete = models.CASCADE)
 
 
-class Internship:
-    internship_id =models.AutoField(primary_key= True)
-    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
-    city_id = models.ForeignKey(City, on_delete=models.CASCADE)
+class Internship(models.Model):
+    internship_id = models.AutoField(primary_key = True)
+    student_id = models.ForeignKey(Student, on_delete = models.CASCADE)
+    city_id = models.ForeignKey(City, on_delete = models.CASCADE)
     date_start = models.DateField()
     date_end = models.DateField()
 
--- DjangoTest/mysite/mysite/views.py
+++ DjangoTest/mysite/mysite/views.py
@@ -3,6 +3,16 @@ from django.http import HttpResponse
 from .forms import AnnuaireForm, EtudiantForm, VilleForm, StageForm
 from .models import Gradyear, Student, Country, City, Internship
 
+# WHERE ... OR ... condition requests
+from django.db.models import Q
+
+
+def index(request):
+    # TODO: faire une requete sur les stages pour
+    # plotter les villes de stage sur la worldmap
+    return render(request, 'index.html')
+
+
 def annuaire(request):
     if request.method == 'GET':
         # create a form instance and populate it
@@ -10,19 +20,45 @@ def annuaire(request):
 
         if form.is_valid():
             # process the data in form.cleaned_data as required
-            # ...
-
+            nom = form.cleaned_data['nom']
+            annee = form.cleaned_data['annee']
+            
+            # make query to get students
+            try:
+                if annee != "":
+                    try:
+                        yearId = Gradyear.objects.get(label = annee)
+                    
+                    except:
+                        return HttpResponse(f'<p>Query failed: no matching gradyear ID</p>')
+                    
+                    students = Student.objects.filter(Q(fname__contains = nom, gradyear_id = yearId) | Q(lname__contains = nom, gradyear_id = yearId))
+                
+                else:
+                    students = Student.objects.filter(Q(fname__contains = nom) | Q(lname__contains = nom))
+            
+            except Student.DoesNotExist:
+                return HttpResponse(f'<p>Query OK: 0 students found in database</p>')
+            
+            # create HttpResponse
+            content = '<p>Query OK: ' + str(len(students)) + ' students found in database.</p><ul>'
+            
+            for i in range(len(students)):
+                # get student data
+                student_id, fname, lname = students[i].student_id, students[i].fname, students[i].lname
+                
+                # add <li> element
+                content += '<li>' + lname.upper() + ' ' + fname.lower() + ' (' + str(student_id) + ')</li>'
+            
+            content += '</ul>'
+            
             # return HttpResponse
-            return render(request, 'annuaire.html', {'form': form})
+            return HttpResponse(content)
 
     else:
         form = AnnuaireForm()
 
     return render(request, 'annuaire.html', {'form': form})
-
-
-def index(request):
-    return render(request, 'index.html')
 
 
 def etudiant(request):
@@ -31,21 +67,34 @@ def etudiant(request):
         form = EtudiantForm(request.POST)
 
         if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            prenom = form.cleaned_data['nom']
-            nom = form.cleaned_data['prenom']
-            try :
-                annee = Gradyear.objects.get(label = form.cleaned_data['annee'] )
-            except Gradyear.DoesNotExist :
-                annee = Gradyear(label = form.cleaned_data['annee'])
-                annee.save()
-            newStudent = Student(fname = prenom, lname=nom, gradyear_id = annee)
-            newStudent.save()           
-            return HttpResponse(f'<h1>new Student : {nom} </h1>')  
+            # get the data from cleaned_data as required
+            prenom = form.cleaned_data['prenom']
+            nom = form.cleaned_data['nom']
+            email = form.cleaned_data['email']
+            annee = form.cleaned_data['annee']
+            
+            # create the gradyear if it does not exist
+            try:
+                newYear = Gradyear.objects.get(label = annee)
+            
+            except Gradyear.DoesNotExist:
+                newYear = Gradyear(label = annee)
+                newYear.save()
+            
+            # create the student if it does not exist
+            try:
+                newStudent = Student.objects.get(email = email)
+            
+            except Student.DoesNotExist:
+                newStudent = Student(fname = prenom, lname = nom, email = email, gradyear_id = newYear)
+                newStudent.save()
+                return HttpResponse(f'<p>Query OK: {prenom.lower()} {nom.upper()} added to database.</p>')
+            
+            return HttpResponse(f'<p>Query failed: mail unicity constraint.</p>')
+    
     else:
         form = EtudiantForm()
-
+    
     return render(request, 'etudiant.html', {'form': form})
 
 
@@ -56,10 +105,29 @@ def ville(request):
 
         if form.is_valid():
             # process the data in form.cleaned_data as required
-            # ...
-
-            # return HttpResponse
-            return render(request, 'ville.html', {'form': form})
+            ville = form.cleaned_data['ville']
+            pays = form.cleaned_data['pays']
+            longitude = form.cleaned_data['long']
+            latitude = form.cleaned_data['lat']
+            
+            # create the country if it does not exist
+            try:
+                newCountry = Country.objects.get(country_name = pays)
+            
+            except Country.DoesNotExist:
+                newCountry = Country(country_name = pays)
+                newCountry.save()
+            
+            # create the city if it does not exist
+            try:
+                newCity = City.objects.get(city_name = ville, country_id = newCountry)
+            
+            except City.DoesNotExist:
+                newCity = City(city_name = ville, longitude = longitude, latitude = latitude, country_id = newCountry)
+                newCity.save()
+                return HttpResponse(f'<p>Query OK: {ville}, {pays} added to database.</p>')
+            
+            return HttpResponse(f'<p>Query failed: unicity constraint.</p>')
 
     else:
         form = VilleForm()
@@ -73,12 +141,55 @@ def stage(request):
         form = StageForm(request.POST)
 
         if form.is_valid():
+            # useless - just for good look
+            prenom = form.cleaned_data['prenom']
+            nom = form.cleaned_data['nom']
+            
             # process the data in form.cleaned_data as required
-            # ...
-
-            # return HttpResponse
-            return render(request, 'stage.html', {'form': form})
-
+            email = form.cleaned_data['email']
+            ville = form.cleaned_data['ville']
+            pays = form.cleaned_data['pays']
+            debut = form.cleaned_data['debut']
+            fin = form.cleaned_data['fin']
+            
+            # date boolean condition check
+            if debut < fin:
+                
+                # get student id
+                try:
+                    student = Student.objects.get(email = email)
+                
+                except Student.DoesNotExist:
+                    return HttpResponse(f'<p>Query failed: no matching student in database.</p>')
+                
+                # get country id
+                try:
+                    country = Country.objects.get(country_name = pays)
+                
+                except Country.DoesNotExist:
+                    return HttpResponse(f'<p>Query failed: no matching country in database.</p>')
+                
+                # get city id
+                try:
+                    city = City.objects.get(city_name = ville, country_id = country)
+                
+                except City.DoesNotExist:
+                    return HttpResponse(f'<p>Query failed: no matching city in database.</p>')
+                
+                # create internship
+                try:
+                    newInternship = Internship.objects.get(student_id = student, city_id = city, date_start = debut, date_end = fin)
+                
+                except Internship.DoesNotExist:
+                    newInternship = Internship(student_id = student, city_id = city, date_start = debut, date_end = fin)
+                    newInternship.save()
+                    return HttpResponse(f'<p>Query OK: internship added to database.</p>')
+                
+                return HttpResponse(f'<p>Query failed: unicity constraint.</p>')
+            
+            else:
+                return HttpResponse(f'<p>Query failed: did you invert your start and end dates?.</p>')
+    
     else:
         form = StageForm()
 
