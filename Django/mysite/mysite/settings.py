b48b065aa6ac7934bfd7f506c555e3f38f8dd3f0
ArnaudGuibert <guibarnaud@gmail.com>
2021-02-15 18:46:53 +0100

    icon pour pages + correction path static relatif

--- Django/mysite/mysite/settings.py
+++ Django/mysite/mysite/settings.py
@@ -11,13 +11,20 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
